1cd0644c1fd303537b8323c24e433093e0004d62
Victor Hugo Benitez Bosques <victorHbenitez85@hotmail.com>
2022-02-27 19:33:23 -0600

    add init project

---
+++ RemindersApp/RemindersApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RemindersApp
+//
+//  Created by Victor Hugo Benitez Bosques on 27/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RemindersApp/RemindersApp/RemindersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersAppApp.swift
+//  RemindersApp
+//
+//  Created by Victor Hugo Benitez Bosques on 27/02/22.
+//
+
+import SwiftUI
+
+@main
+struct RemindersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
